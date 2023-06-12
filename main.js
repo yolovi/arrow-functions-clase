da8161ee9b9deaadb2f525bcbba3c98769eab80c
yolovi <yolandalopevi@gmail.com>
2023-06-12 12:09:49 +0200

    fist commit funciones flecha

---
+++ main.js
@@ -0,0 +1,43 @@
+// 1. Funciones flecha
+//[OK] // Convierte la siguiente función en una función flecha:
+// function saludar() {
+//   return "Hola";
+// }
+
+const saludar = () => "Hola";
+console.log(saludar)
+
+//[OK] // Convierte la siguiente función en una función flecha en línea:
+// function division(a,b) {
+//   return a / b;
+// }
+
+const division = (a,b) => a/b;
+console.log(division(4,2))
+
+//[OK] // Convierte la siguiente función en una función flecha:
+// function miNombre(nombre) {
+//   return `Mi nombre es ${nombre}`;
+// }
+
+const miNombre = (nombre) =>  `Mi nombre es ${nombre}`;
+console.log(miNombre("Yol"))
+
+//[OK] // Convierte las siguientes funciones en funciones flecha:
+// function test2() {
+//   console.log("Función test 2 ejecutada.");
+// }
+// function test1(callback) {
+//   callback();
+// }	
+
+// test2()
+// test1(test2)
+
+
+const testDos = () => console.log("Función test 2 ejecutada.");
+testDos()
+
+const testUno = (callback) => callback();
+testUno(test2)
+
